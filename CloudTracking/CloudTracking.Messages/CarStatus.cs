a7c8fab857af1884311a037f66417af262011c33
amr azab <amrazab@windowslive.com>
2019-07-19 01:52:30 +0200

    Adding Messages Projecct

---
+++ CloudTracking/CloudTracking.Messages/CarStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudTracking.Messages
+{
+    public enum CarStatus
+    {
+        Working =1,
+        Error = 2,
+    }
+}
---
+++ CloudTracking/CloudTracking.Messages/PingMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CloudTracking.Messages
+{
+    public class PingMessage
+    {
+        public DateTime Time { get; set; }
+        public string CarId { get; set; }
+        public string CustomerId { get; set; }
+        public int OilLevel { get; set; }
+        public int Speed { get; set; }
+        public CarStatus CarStatus { get; set; }
+        public string  ErrorMessage {get;set;}
+
+    }
+}
