9b63e3d582f7e2e7956d6bb131e0f5ebe527652d
amr azab <amrazab@windowslive.com>
2019-07-19 04:00:57 +0200

    add car simulator Project

---
+++ CloudTracking/CloudTraking.CarSimulator/Program.cs
@@ -0,0 +1,43 @@
+﻿using CloudTracking.Messages;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+
+namespace CloudTraking.CarSimulator
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            Timer t = new Timer(ping,null,0,60000);
+            Console.ReadLine();
+        }
+
+        private static void ping(object state)
+        {
+            try
+            {
+                Console.WriteLine("ping started");
+                PingMessage pingMessage = new PingMessage() {
+                    Time = DateTime.Now,
+                CarId = "tst id",
+                CarStatus = CarStatus.Working,
+                CustomerId = "c Id",
+                ErrorMessage = "",
+                OilLevel = 10,
+                Speed=100
+                };
+                var jsonn = JsonConvert.SerializeObject(pingMessage);
+                client.PostAsync("http://localhost:59426/api/Ping", new StringContent(jsonn, Encoding.UTF8, "application/json"));
+                Console.WriteLine("sent....");
+            }
+            catch (Exception ex)
+            {
+                Console.Write("error while pingin the service : " + ex.Message);
+            }
+        }
+    }
+}
---
+++ CloudTracking/CloudTraking.PingGateWay/Controllers/PingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudTracking.Messages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudTraking.PingGateWay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Ping GateWay";
+        }
+
+      
+        [HttpPost]
+        public ActionResult<string> Post([FromBody]PingMessage pingMessage)
+        {
+            return "value";
+        }
+
+    }
+}
